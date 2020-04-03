644e9c8a35218db5421b669b98a782130de94657
Kissy1999 <35816345+Kissy1999@users.noreply.github.com>
2020-04-03 13:56:33 -0400

    Added scene and CSS

--- Scripts/config/screen.js
+++ Scripts/config/screen.js
@@ -1,2 +1,15 @@
 "use strict";
+var config;
+(function (config) {
+    var Screen = /** @class */ (function () {
+        function Screen() {
+        }
+        Screen.WIDTH = 800;
+        Screen.HEIGHT = 800;
+        Screen.HALF_WIDTH = 400;
+        Screen.HALF_HEIGHT = 400;
+        return Screen;
+    }());
+    config.Screen = Screen;
+})(config || (config = {}));
 //# sourceMappingURL=screen.js.map
---
+++ Scripts/config/screen.ts
@@ -0,0 +1,8 @@
+module config {
+    export class Screen {
+        public static WIDTH:number = 800;
+        public static HEIGHT:number = 800;
+        public static HALF_WIDTH:number = 400;
+        public static HALF_HEIGHT:number = 400;
+    }
+}
