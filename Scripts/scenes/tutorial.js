c6ad4cdc2e228ad12fc149438969d4d36247b426
Kissy1999 <35816345+Kissy1999@users.noreply.github.com>
2020-04-03 14:04:30 -0400

    Added Instruction page

--- Scripts/scenes/tutorial.js
+++ Scripts/scenes/tutorial.js
@@ -1,2 +1,54 @@
 "use strict";
+// tutirial section.
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var scenes;
+(function (scenes) {
+    var Tutorial = /** @class */ (function (_super) {
+        __extends(Tutorial, _super);
+        // constructors
+        function Tutorial() {
+            var _this = _super.call(this) || this;
+            _this.Start();
+            return _this;
+        }
+        // private methods
+        // public methods
+        Tutorial.prototype.Start = function () {
+            this._background = new createjs.Bitmap(managers.Game.AssetManager.getResult("tutorial"));
+            this._startButton = new createjs.Bitmap(managers.Game.AssetManager.getResult("playbutton"));
+            this._startButton.x = 650;
+            this._startButton.y = 470;
+            this.Main();
+        };
+        Tutorial.prototype.Update = function () {
+        };
+        Tutorial.prototype.Reset = function () {
+        };
+        Tutorial.prototype.Destroy = function () {
+            this.removeAllChildren();
+        };
+        Tutorial.prototype.Main = function () {
+            console.log("Tutorial - Tutorial");
+            this.addChild(this._background);
+            this.addChild(this._startButton);
+            this._startButton.on("click", function () {
+                managers.Game.CurrentState = config.Scene.PLAY;
+            }, this);
+        };
+        return Tutorial;
+    }(objects.Scene));
+    scenes.Tutorial = Tutorial;
+})(scenes || (scenes = {}));
 //# sourceMappingURL=tutorial.js.map
---
+++ Scripts/scenes/tutorial.ts
@@ -0,0 +1,57 @@
+// tutirial section.
+
+module scenes {
+    export class Tutorial extends objects.Scene{
+
+        // member variables
+        private _background: createjs.Bitmap;
+        private _startButton: createjs.Bitmap;
+
+
+        
+        // constructors
+        constructor() {
+            super();
+
+            this.Start();
+        }
+
+        // private methods
+
+        // public methods
+        public Start():void {
+
+            this._background = new createjs.Bitmap(managers.Game.AssetManager.getResult("tutorial"));
+            this._startButton = new createjs.Bitmap(managers.Game.AssetManager.getResult("playbutton"));
+            this._startButton.x = 650;
+            this._startButton.y = 470;
+           
+            this.Main();
+        }
+
+        public Update():void {
+         
+        }
+
+        public Reset():void {
+
+        }
+
+        public Destroy():void {
+            this.removeAllChildren();
+        }
+
+        public Main():void {
+            console.log(`Tutorial - Tutorial`);
+            
+            this.addChild(this._background);
+            this.addChild(this._startButton);
+
+            this._startButton.on("click", function(){
+                managers.Game.CurrentState = config.Scene.PLAY;
+            }, this);
+          
+        }
+    }
+
+}
