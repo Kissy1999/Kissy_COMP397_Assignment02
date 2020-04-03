536e62af5d560ac5d50f939f80f30a9bd4555e78
Kissy1999 <35816345+Kissy1999@users.noreply.github.com>
2020-04-03 14:02:14 -0400

    Added play scene

--- Scripts/scenes/play.js
+++ Scripts/scenes/play.js
@@ -1,2 +1,96 @@
 "use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var scenes;
+(function (scenes) {
+    var Play = /** @class */ (function (_super) {
+        __extends(Play, _super);
+        // constructors
+        function Play() {
+            var _this = _super.call(this) || this;
+            _this.Start();
+            return _this;
+        }
+        // private methods
+        Play.prototype._buildBears = function () {
+            for (var count = 0; count < this._bearNum; count++) {
+                this._bears.push(new objects.Bear());
+                //this._clouds[count] = new objects.Cloud();
+            }
+        };
+        Play.prototype._buildPoints = function () {
+            for (var count = 0; count < this._pointNum; count++) {
+                this._point.push(new objects.Point());
+                //this._clouds[count] = new objects.Cloud();
+            }
+        };
+        // public methods
+        Play.prototype.Start = function () {
+            this.engineSound = createjs.Sound.play("mainBgm");
+            this.engineSound.loop = -1;
+            this.engineSound.volume = 0.2;
+            this._hero = new objects.Hero();
+            this._iceland = new objects.Iceland();
+            this._point = new Array();
+            this._pointNum = 2;
+            this._buildPoints();
+            // creates an empty array of type Cloud
+            this._bears = new Array();
+            this._bearNum = 5;
+            this._buildBears();
+            this.Main();
+        };
+        Play.prototype.Update = function () {
+            var _this = this;
+            this._hero.Update();
+            this._iceland.Update();
+            this._point.forEach(function (point) {
+                point.Update();
+                managers.Collision.check(_this._hero, point);
+            });
+            this._bears.forEach(function (bear) {
+                bear.Update();
+                managers.Collision.check(_this._hero, bear);
+            });
+        };
+        Play.prototype.Reset = function () {
+        };
+        Play.prototype.Destroy = function () {
+            this.engineSound.stop();
+            this.removeAllChildren();
+        };
+        Play.prototype.Main = function () {
+            console.log("Starting - PLAY SCENE");
+            // adding the ocean to the scene
+            this.addChild(this._iceland);
+            // adding the island to the scene
+            for (var _i = 0, _a = this._point; _i < _a.length; _i++) {
+                var point = _a[_i];
+                this.addChild(point);
+            }
+            // adding the plane to the scene
+            this.addChild(this._hero);
+            // adding the cloud to the scene
+            for (var _b = 0, _c = this._bears; _b < _c.length; _b++) {
+                var bear = _c[_b];
+                this.addChild(bear);
+            }
+            this.addChild(managers.Game.ScoreBoardManager.LivesLabel);
+            this.addChild(managers.Game.ScoreBoardManager.ScoreLabel);
+        };
+        return Play;
+    }(objects.Scene));
+    scenes.Play = Play;
+})(scenes || (scenes = {}));
 //# sourceMappingURL=play.js.map
---
+++ Scripts/scenes/play.ts
@@ -0,0 +1,119 @@
+module scenes {
+    export class Play extends objects.Scene {
+        // member variables
+      
+        private _iceland:objects.Iceland;
+        
+        private _hero:objects.Hero;
+       
+        private _bears:objects.Bear[];
+        private _bearNum:number;
+       
+        private _point:objects.Point[];
+        private _pointNum:number;
+        
+        public engineSound:createjs.AbstractSoundInstance;
+
+        // constructors
+        constructor() {
+            super();
+
+            this.Start();
+        }
+        // private methods
+    
+
+        private _buildBears():void {
+            for (let count = 0; count < this._bearNum; count++) {
+                this._bears.push(new objects.Bear());
+                //this._clouds[count] = new objects.Cloud();
+            }
+        }
+
+        private _buildPoints():void {
+            for (let count = 0; count < this._pointNum; count++) {
+                this._point.push(new objects.Point());
+                //this._clouds[count] = new objects.Cloud();
+            }
+        }
+
+        // public methods
+        public Start():void {
+            this.engineSound = createjs.Sound.play("mainBgm");
+            this.engineSound.loop = -1;
+            this.engineSound.volume = 0.2;
+
+
+            
+            this._hero = new objects.Hero();
+            this._iceland = new objects.Iceland();
+
+            this._point = new  Array <objects.Point>();
+            this._pointNum = 2;
+
+            this._buildPoints();
+           
+
+            // creates an empty array of type Cloud
+            this._bears = new Array <objects.Bear>();
+         
+            this._bearNum = 5;
+            
+            this._buildBears();
+           
+            this.Main();
+        }
+
+        public Update():void {
+            this._hero.Update();
+            this._iceland.Update();
+      
+
+            this._point.forEach(point => {
+                point.Update();
+                managers.Collision.check(this._hero, point);
+            });
+
+           
+
+            this._bears.forEach(bear => {
+                bear.Update();
+                managers.Collision.check(this._hero, bear);
+            });
+            
+        }
+
+        public Reset():void {
+
+        }
+
+        public Destroy():void {
+            this.engineSound.stop();
+            this.removeAllChildren();
+        }
+
+        public Main():void {
+            console.log(`Starting - PLAY SCENE`);
+
+            // adding the ocean to the scene
+            this.addChild(this._iceland);            
+
+            // adding the island to the scene
+            for (const point of this._point) {
+                this.addChild(point);
+            }
+
+            // adding the plane to the scene
+            this.addChild(this._hero);
+
+            // adding the cloud to the scene
+     
+
+            for (const bear of this._bears) {
+                this.addChild(bear);
+            }
+
+            this.addChild(managers.Game.ScoreBoardManager.LivesLabel);
+            this.addChild(managers.Game.ScoreBoardManager.ScoreLabel);
+        }
+    }
